c49281397fded4ed5b60d828b111231a27274fbe
John Rodriguez <jrod@squareup.com>
2018-06-25 14:10:40 -0400

    Add MainActivity basics

---
+++ app/src/main/java/nyc/c4q/vice/mobile/ui/MainActivity.java
@@ -0,0 +1,13 @@
+package nyc.c4q.vice.mobile.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import nyc.c4q.vice.mobile.R;
+
+public class MainActivity extends AppCompatActivity {
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
