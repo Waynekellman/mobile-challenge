0b91de6aa1b27d2f385ce9628e8afb85e55a764e
John Rodriguez <jrod@squareup.com>
2018-07-07 04:02:39 -0400

    Replace ActionBar with Toolbar

--- app/src/main/java/nyc/c4q/vice/mobile/ui/MainActivity.java
+++ app/src/main/java/nyc/c4q/vice/mobile/ui/MainActivity.java
@@ -3,8 +3,8 @@ package nyc.c4q.vice.mobile.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,28 +13,26 @@ import butterknife.ButterKnife;
 import nyc.c4q.vice.mobile.R;
 
 public class MainActivity extends AppCompatActivity {
+  @BindView(R.id.toolbar) Toolbar toolbar;
   @BindView(R.id.container) ViewGroup container;
   @BindView(R.id.bottom_navigation) BottomNavigationView bottomNavigationView;
 
   private View homeView;
   private View favoritesView;
-  private ActionBar actionBar;
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     ButterKnife.bind(this);
 
-    actionBar = getSupportActionBar();
-
     LayoutInflater layoutInflater = getLayoutInflater();
 
     homeView = layoutInflater.inflate(R.layout.view_home, null);
     favoritesView = layoutInflater.inflate(R.layout.view_favorites, null);
 
-    // set the initial view and action bar text
+    // set the initial view and toolbar text
     container.addView(homeView);
-    actionBar.setTitle(R.string.home_title);
+    toolbar.setTitle(R.string.home_title);
 
     // logic to switch views upon tab click
     bottomNavigationView.setOnNavigationItemSelectedListener(menuItem -> {
@@ -43,12 +41,12 @@ public class MainActivity extends AppCompatActivity {
         case R.id.action_home:
           container.removeAllViews();
           container.addView(homeView);
-          actionBar.setTitle(R.string.home_title);
+          toolbar.setTitle(R.string.home_title);
           break;
         case R.id.action_favorites:
           container.removeAllViews();
           container.addView(favoritesView);
-          actionBar.setTitle(R.string.favorites_title);
+          toolbar.setTitle(R.string.favorites_title);
           break;
         default:
           throw new IllegalStateException(
